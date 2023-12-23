9189b930813d027f27be08627591b79f14647b2e
E-know <eknow25@kakao.com>
2023-12-23 15:38:00 +0900

    Initial Commit

---
+++ Alumni Committee/Alumni_CommitteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alumni_CommitteeApp.swift
+//  Alumni Committee
+//
+//  Created by Inho Choi on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Alumni_CommitteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
